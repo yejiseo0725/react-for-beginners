ebb82e5ce1a7fd707a105b2af40fbc063a42eb3b
YEJI SEO <yejiseo0725@gmail.com>
2026-09-03 09:39:35 +0900

    Vite@ setting

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <div>
+          <h1>Welcome Back@@@</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis minus repellendus sed beatae aut eum quibusdam molestiae cumque blanditiis ea quo suscipit consequatur animi rem, molestias voluptate assumenda totam nesciunt labore nam, ipsum, ad velit vero? Pariatur doloribus nam, inventore maxime quam earum voluptates ducimus ex modi, aliquid voluptatum? Placeat?
+          </p>
+        </div>
+      </section>
+    </>
+  )
+  
+}
+
+export default App;
