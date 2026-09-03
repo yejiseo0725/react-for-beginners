bb24ca08dd26cd17b5f8185723e4f455f6ae0afc
YEJI SEO <yejiseo0725@gmail.com>
2026-09-03 11:58:50 +0900

    props - css component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import './App.css'
+import Button from './Button'
+import styles from './App.module.css'
 
 function App() {
 
@@ -7,15 +8,13 @@ function App() {
     <>
       <section>
         <div>
-          <h1>Welcome Back@@@</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis minus repellendus sed beatae aut eum quibusdam molestiae cumque blanditiis ea quo suscipit consequatur animi rem, molestias voluptate assumenda totam nesciunt labore nam, ipsum, ad velit vero? Pariatur doloribus nam, inventore maxime quam earum voluptates ducimus ex modi, aliquid voluptatum? Placeat?
-          </p>
+          <h1 className={styles.title}>Welcome Back@@@</h1>
+          <Button text={"귀요미"} />
         </div>
       </section>
     </>
   )
-  
+
 }
 
 export default App;
