b9f57518f8b915bc7862072f793a7d39f2fbd814
keroppi <yejiseo0725@gmail.com>
2026-09-03 21:13:37 +0900

    to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import { useState, useEffect } from 'react'
-
-function Hello() {
-  
-  function byFn() {
-    console.log("bye :<");
-  }
-
-  function hiFn() {
-    console.log("created :3 :3 :3");
-    return byFn;
-  }
-
-  useEffect(hiFn, []);
-  return <h1>HELLO :3</h1>  
-}
+import { useState, useEffect } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
 
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
 
   return (
-  <div>
-    {showing ? <Hello /> : null}
-    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-  </div>
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Write your to do...'
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
   );
 }
 
