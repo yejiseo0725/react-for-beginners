b8003ac7da2c9ed3b464bf7137ecd8d7fdb7af99
keroppi <yejiseo0725@gmail.com>
2026-09-03 21:28:58 +0900

    showing to do list

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,10 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
