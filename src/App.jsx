10a3ca2b0e1f079532aacec0e32610280159356c
YEJI SEO <yejiseo0725@gmail.com>
2026-09-03 15:05:54 +0900

    useEffect [ ] 사용하기

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,27 @@ import styles from './App.module.css'
 function App() {
 
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('i run all the time ---!');
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log("CALL THE API@@@");
+    console.log("I run ONLY ONCE!");
   }, []);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes...");
+  }, [counter]);
 
   return (
     <>
         <div>
+          <input value={keyword} onChange={onChange} type="text" placeholder="Search Here..." />
           <h1>{counter}</h1>
           <button onClick={onClick}>Click me</button>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 // == index.js from CRA
 
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <App />,
 )
