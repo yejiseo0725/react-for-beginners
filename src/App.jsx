0806eb0d66720061fc4dfd296106590a9ba822c3
YEJI SEO <yejiseo0725@gmail.com>
2026-09-04 14:02:58 +0900

    movie app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async() => {
-    const response = await fetch(`https://movies-api.accel.li/api/v2/list_movies.json?sort_by=year`);
-    const json = await response.json();
+    const json = await (await fetch(`https://movies-api.accel.li/api/v2/list_movies.json?sort_by=year`
+    )).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
@@ -13,11 +13,27 @@ function App() {
   useEffect(() => {
     getMovies()
   }, []);
+
+  console.log(movies);
   
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : null}
-    </div>
+      {loading ? <h1>Loading...</h1> : 
+      <div>
+        {movies.map(movie => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} alt="movie-cover-img" />
+            <h2>{movie.title}</h2>
+            <strong>{movie.year}</strong>
+            <p>{movie.summary}</p>
+            <ul>
+              <li>{movie.genres.map(g => <li key={g}>{g}</li>)}</li>
+            </ul>
+          </div>
+        ))}
+        </div>
+      }
+      </div>
   )
 }
 
