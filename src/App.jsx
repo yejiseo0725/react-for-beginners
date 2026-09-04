a099131bbee57ce8b15b9d021ed3609af2806ea4
YEJI SEO <yejiseo0725@gmail.com>
2026-09-04 11:58:03 +0900

    moives API 받아오기

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,21 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]); // 빈 값 주는 이유: undefined 방지
-  
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-        .then(response => response.json())
-        .then((json) => {
-            setCoins(json);
-            setLoading(false);
-        });
-}, []);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch(`https://movies-api.accel.li/api/v2/list_movies.json?sort_by=year`);
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
 
+  useEffect(() => {
+    getMovies()
+  }, []);
+  
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => 
-          <option>
-            {coin.name} | ({coin.symbol}): {coin.quotes.USD.price} USD
-          </option>
-        )}
-        </select>
-      )}
+      {loading ? <h1>Loading...</h1> : null}
     </div>
   )
 }
